8b3c97272d99e8dc3fc3fbb6edb5f3273f18777c
HeniNechi <32527869+HeniNechi@users.noreply.github.com>
2019-09-30 06:25:28 +0100

    Delete store.js

--- components/store.js
+++
@@ -1,4 +0,0 @@
-//import {createStore} from ' redux';
-//import thunk from 'redux-thunk';
-//const initialState = {};
-//export default createStore(rootReducer,initialState, applyMiddleware(thunk));
